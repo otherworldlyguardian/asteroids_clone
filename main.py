a48002237ee16ec9d7b9cef00ec3e18d17336457
otherworldlyguardian <otherworldlyguardian@gmail.com>
2024-09-11 16:23:30 -0400

    E: Asteroid generation logic

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    updatable_group, drawable_group, asteroids_group = pygame.sprite.Group(), pygame.sprite.Group(), pygame.sprite.Group()
+    Player.containers = (updatable_group, drawable_group)
+    Asteroid.containers = (asteroids_group, updatable_group, drawable_group)
+    AsteroidField.containers = (updatable_group)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroids_field = AsteroidField()
     dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        for object in updatable_group:
+            object.update(dt)
+        for object in drawable_group:
+            object.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
