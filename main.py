454e8afad49768e0df48bd4ec30f8220675a7b22
otherworldlyguardian <otherworldlyguardian@gmail.com>
2024-09-11 20:26:21 -0400

    F: Bullets and player collision

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
+import sys
 import pygame
 from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    updatable_group, drawable_group, asteroids_group = pygame.sprite.Group(), pygame.sprite.Group(), pygame.sprite.Group()
+    updatable_group = pygame.sprite.Group()
+    drawable_group = pygame.sprite.Group()
+    asteroids_group = pygame.sprite.Group()
+    shots_group = pygame.sprite.Group()
     Player.containers = (updatable_group, drawable_group)
     Asteroid.containers = (asteroids_group, updatable_group, drawable_group)
     AsteroidField.containers = (updatable_group)
+    Shot.containers = (shots_group, updatable_group, drawable_group)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroids_field = AsteroidField()
     dt = 0
@@ -22,6 +28,9 @@ def main():
         screen.fill("black")
         for object in updatable_group:
             object.update(dt)
+        for asteroid in asteroids_group:
+            if asteroid.collision(player):
+                sys.exit("Game over!")
         for object in drawable_group:
             object.draw(screen)
         pygame.display.flip()
